24aa98ac91c124fa1ca85c6f5fdbc531a29d7018
Danny van Heumen <danny@dannyvanheumen.nl>
2020-02-04 17:33:32 +0100

    Added program flag for loading blocklist into proxy/relay.

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"golang.org/x/net/proxy"
 
@@ -13,9 +14,21 @@ import (
 func main() {
 	listenAddr := flag.String("listen", ":8080", "Listening address and port for HTTP relay proxy.")
 	blockedAddrs := flag.String("block", "", "Comma-separated list of blocked host names, zone names, ip addresses and CIDR addresses.")
+	blocklist := flag.String("blocklist", "", "Filename referring to a hosts-formatted blocklist. (e.g. from energized.pro)")
 	flag.Parse()
 	// Prepare proxy dialer
 	var dialer proxy.Dialer = proxy.Direct
+	if *blocklist != "" {
+		listfile, err := os.Open(*blocklist)
+		if err != nil {
+			panic("Failed to access blocklist: " + *blocklist)
+		}
+		blocklistDialer := httpRelay.BlocklistDialer{List: make(map[string]struct{}, 0), Dialer: dialer}
+		if err := blocklistDialer.Load(listfile); err != nil {
+			panic("Failed to load content into blocklist dialer: " + err.Error())
+		}
+		dialer = &blocklistDialer
+	}
 	if *blockedAddrs != "" {
 		// Prepare dialer for blocked addresses
 		perHostDialer := proxy.NewPerHost(dialer, &httpRelay.NopDialer{})
--- cmd/relay/main.go
+++ cmd/relay/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/cobratbq/httpRelay"
 
@@ -14,12 +15,24 @@ func main() {
 	socksAddr := flag.String("socks", "localhost:8000", "Address and port of SOCKS5 proxy server.")
 	listenAddr := flag.String("listen", ":8080", "Listening address and port for HTTP relay proxy.")
 	blockedAddrs := flag.String("block", "", "Comma-separated list of blocked host names, zone names, ip addresses and CIDR addresses.")
+	blocklist := flag.String("blocklist", "", "Filename referring to a hosts-formatted blocklist. (e.g. from energized.pro)")
 	flag.Parse()
 	// Prepare proxy relay with target SOCKS proxy
 	dialer, err := proxy.SOCKS5("tcp", *socksAddr, nil, proxy.Direct)
 	if err != nil {
 		log.Println("Failed to create proxy definition:", err.Error())
 		return
+	}
+	if *blocklist != "" {
+		listfile, err := os.Open(*blocklist)
+		if err != nil {
+			panic("Failed to access blocklist: " + *blocklist)
+		}
+		blocklistDialer := httpRelay.BlocklistDialer{List: make(map[string]struct{}, 0), Dialer: dialer}
+		if err := blocklistDialer.Load(listfile); err != nil {
+			panic("Failed to load content into blocklist dialer: " + err.Error())
+		}
+		dialer = &blocklistDialer
 	}
 	if *blockedAddrs != "" {
 		// Prepare dialer for blocked addresses
