b88fc56995ac2db5150541caff716b28615f846b
Danny van Heumen <danny@dannyvanheumen.nl>
2020-02-04 22:16:19 +0100

    Add comments to satisfy lint.

--- nopdialer.go
+++ nopdialer.go
@@ -5,10 +5,13 @@ import (
 	"net"
 )
 
+// ErrBlockedHost indicates that host is blocked.
 var ErrBlockedHost = errors.New("host is blocked")
 
+// NopDialer does not perform dialing operation as host is blocked.
 type NopDialer struct{}
 
+// Dial performs no-op dial operation.
 func (NopDialer) Dial(network, addr string) (net.Conn, error) {
 	return nil, ErrBlockedHost
 }
